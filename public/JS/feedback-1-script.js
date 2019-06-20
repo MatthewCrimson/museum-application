9bf8ea41634282c288c544e6a8998da6068632a4
Matthijs <matthijs.vogel@Live.nl>
2019-06-20 19:09:15 +0200

    further implementation of database interaction

--- public/JS/feedback-1-script.js
+++ public/JS/feedback-1-script.js
@@ -5,13 +5,25 @@ $('#submitbutton1').click(function () {
 
   var selected_list = $("select").data('picker').selected_values();
 
-  var x = 10;
+  //var database = firebase.database();
 
-  var database = firebase.database();
+  var i;
+  for (i = 0; i < selected_list.length; i++)
+  {
+    var old_value;
+    firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).once('value', function(snapshot){
+      old_value = snapshot.val();
+      console.log(old_value);
+    });
 
-  var updates = {};
-  updates['/Votes/Gallery1/item1'] = 10;
-  firebase.database().ref("/Gallery1/item1").set({ value: 10});
+    firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).set(old_value + 1);
+    console.log("attempted to send")
+  }
+
+
+  //var updates = {};
+  //updates['/Votes/Gallery1/item1'] = 10;
+  //firebase.database().ref("Votes/Gallery1/item1").set(+1);
 
   //$("select").data('picker').destroy();
   //window.location.href = "gallery2.html";
