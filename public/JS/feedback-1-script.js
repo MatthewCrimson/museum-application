c536adf405fb451177a4b28b56810d3ee1b97b9c
Matthijs <matthijs.vogel@Live.nl>
2019-06-20 17:25:13 +0200

    Update feedback-1-script.js

--- public/JS/feedback-1-script.js
+++ public/JS/feedback-1-script.js
@@ -4,14 +4,15 @@ $('#submitbutton1').click(function () {
   console.log("selected works: " + $("select").data('picker').selected_values());
 
   var selected_list = $("select").data('picker').selected_values();
-  alert(selected_list);
 
-  var db = firebase.database();
+  var x = 10;
+
+  var database = firebase.database();
 
   var updates = {};
   updates['/Votes/Gallery1/item1'] = 10;
-  firebase.database().ref("/Gallery1/item1").set(10);
+  firebase.database().ref("/Gallery1/item1").set({ value: 10});
 
-  $("select").data('picker').destroy();
-  window.location.href = "gallery2.html";
+  //$("select").data('picker').destroy();
+  //window.location.href = "gallery2.html";
 });
