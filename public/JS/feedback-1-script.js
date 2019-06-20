f1dcba50a200ec79cc373ef192bfbda4e3f123ac
LeaveItToCharge <matthijs.vogel@Live.nl>
2019-06-20 12:48:33 +0200

    attempted to implement sending of data

--- public/JS/feedback-1-script.js
+++ public/JS/feedback-1-script.js
@@ -1,9 +1,17 @@
 $("select").imagepicker()
 
-$
-
 $('#submitbutton1').click(function () {
   console.log("selected works: " + $("select").data('picker').selected_values());
+
+  var selected_list = $("select").data('picker').selected_values();
+  alert(selected_list);
+
+  var db = firebase.database();
+
+  var updates = {};
+  updates['/Votes/Gallery1/item1'] = 10;
+  firebase.database().ref("/Gallery1/item1").set(10);
+
   $("select").data('picker').destroy();
   window.location.href = "gallery2.html";
 });
