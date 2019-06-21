a3477d751d8890688b3d31482d30324e35230f17
Matthijs <matthijs.vogel@Live.nl>
2019-06-21 10:03:39 +0200

    sawed off

--- public/JS/feedback-1-script.js
+++ public/JS/feedback-1-script.js
@@ -11,19 +11,18 @@ $('#submitbutton1').click(function () {
   for (i = 0; i < selected_list.length; i++)
   {
     var old_value;
-    firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).once('value', function(snapshot){
+    firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).on('value', function(snapshot){
       old_value = snapshot.val();
-      console.log(old_value);
+      console.log("old value: " + old_value);
+      updateValues(old_value, selected_list[i]);
+
+      firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).set(old_value + 1);
+      console.log("attempted to send")
     });
 
-    firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).set(old_value + 1);
-    console.log("attempted to send")
+    //firebase.database().ref("Votes/Gallery1/item" + selected_list[i]).set(old_value + 1);
+  //  console.log("attempted to send lower_old value:" +old_value);
   }
-
-
-  //var updates = {};
-  //updates['/Votes/Gallery1/item1'] = 10;
-  //firebase.database().ref("Votes/Gallery1/item1").set(+1);
 
   //$("select").data('picker').destroy();
   //window.location.href = "gallery2.html";
